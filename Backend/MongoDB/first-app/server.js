a5d8c95f29396d45dd6bdb89034d189829964155
Manas Ranjan Bera <manasbera484@gmail.com>
2026-04-09 16:33:17 +0530

    mongodb

---
+++ Backend/MongoDB/first-app/server.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+//! Express app
+const app = express();
+
+
+//? Schema
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    description: String,
+    rating: Number
+})
+
+const Product = mongoose.model("Product", productSchema);
+
+//? Create a new product
+const product1 = new Product({
+    name: "iPhone 14 Pro Max",
+    price: 1099,
+    description: "The latest iPhone with advanced features and improved performance.",
+    rating: 4.8
+})
+// product1.save()
+
+// Product.create({
+//     name: "Samsung Galaxy S23 Ultra",
+//     price: 1199,
+//     description: "The latest Samsung flagship phone with a stunning display and powerful performance.",
+//     rating: 4.7
+// })
+
+
+//? Get all products
+// Product.find()
+//     .then((res) => {
+//         console.log("All the products", res);
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+
+//? Get single product
+// Product.findOne({ price: 1099 })
+//     .then((res) => {
+//         console.log("Pruct with price- 1099", res);
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+
+// Product.findById("69d7798be7bbef9f45527bbe")
+//     .then((res) => {
+//         console.log("Pruct with unique id- ", res);
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+
+//? Update Product
+// Product.updateOne({ name: "Samsung Galaxy S23 Ultra" }, { price: 25000 })
+//     .then((res) => {
+//         console.log("Product Updated");
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+
+// Product.updateMany({ name: "Samsung Galaxy S23 Ultra" }, { price: 25000 })
+//     .then((res) => {
+//         console.log("All Product Updated");
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+
+
+//? Delete a Product
+// Product.findByIdAndDelete("69d779446f01c047cbd418d5")
+//     .then((res) => {
+//         console.log("Product Deleted");
+//     }).catch((err) => {
+//         console.log(err);
+//     })
+Product.deleteOne({ name: "Samsung Galaxy S23 Ultra" })
+    .then((res) => {
+        console.log("Product Deleted");
+    }).catch((err) => {
+        console.log(err);
+    })
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then((res) => {
+        console.log("DB Connected");
+
+    }).catch((err) => {
+        console.log(err);
+    })
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+})
